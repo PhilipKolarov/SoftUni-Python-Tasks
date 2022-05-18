4b409dd8f1d2e1176c2a54e90ba5d17b5d06dd17
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-05-18 09:25:42 +0300

    Add file
    A code that checks in the parentheses in a string are properly matched

---
+++ balanced_parantheses.py
@@ -0,0 +1,24 @@
+expression = input()
+
+opening_brackets = []
+
+pairs = {'(': ')', '[': ']', '{': '}'}
+
+balanced = True
+for ch in expression:
+    if ch in '({[':
+        opening_brackets.append(ch)
+    elif not opening_brackets:
+        balanced = False
+    else:
+        final_opening_brackets = opening_brackets.pop()
+        if pairs[final_opening_brackets] != ch:
+            balanced = False
+
+    if not balanced:
+        break
+
+if not balanced or opening_brackets != []:
+    print("NO")
+else:
+    print("YES")
