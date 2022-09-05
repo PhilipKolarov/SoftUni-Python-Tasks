e761aae67611961833d6d094c07b38d1229be5b2
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-09-05 11:00:40 +0300

    Add files via upload

---
+++ reverse_iter.py
@@ -0,0 +1,16 @@
+class reverse_iter:
+    def __init__(self, values):
+        self.values = list(values)
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < -len(self.values):
+            raise StopIteration
+
+        value_to_return = self.values[self.index]
+        self.index -= 1
+        return value_to_return
+
