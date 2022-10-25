c90da037035ff5814985c57cc632a1f757ab03fb
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-10-25 09:59:00 +0300

    Add files via upload

---
+++ logged.py
@@ -0,0 +1,15 @@
+def logged(func):
+    def wrapper(*args):
+        result = func(*args)
+        return f"you called {func.__name__}{args}\nit returned {result}"
+
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+
+@logged
+def sum_func(a, b):
+    return a + b
