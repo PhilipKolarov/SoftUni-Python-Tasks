5ccd9f3c7f14208aac2fe313a0ec854d71949885
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-08-04 13:27:31 +0300

    Delete flatten_lists.py

--- flatten_lists.py
+++
@@ -1,8 +0,0 @@
-input_lists = input().split('|')
-output = []
-
-for i in range(len(input_lists) - 1, -1, -1):
-    current_elements = input_lists[i].strip().split()
-    output.extend(current_elements)
-
-print(' '.join(output))
